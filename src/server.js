16cf47e2d653f12fce4b214da35c4447177c8b9a
Dawn McNeil <dawn.a.mcneil@gmail.com>
2019-11-07 19:11:15 -0500

    update server file

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
-const app = require('./app');
-const {PORT} = require('./config');
+const express = require('express');
+ const app = express();
 
+ const PORT = process.env.PORT || 3000;
 
+ app.get('/api/*', (req, res) => {
+   res.json({ok: true});
+ });
 
-app.listen(PORT, () =>{
-    console.log(`Server listening at http://localhost:${PORT}`);
-});
+ app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+ module.exports = {app};
