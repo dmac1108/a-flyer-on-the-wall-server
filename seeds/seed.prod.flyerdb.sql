f2974150afe76e4486a22ec5c060fec948da4bf8
Dawn McNeil <dawn.a.mcneil@gmail.com>
2019-11-30 07:11:54 -0500

    update for production

---
+++ seeds/seed.prod.flyerdb.sql
@@ -0,0 +1,24 @@
+INSERT INTO users (firstname, lastname, email, username, user_password)
+VALUES
+('Jane', 'Smith', 'jane.smith@test.com', 'jsmith', '$2a$12$H/xB3BNGPBemIPx2LJw4HuIEwvV98zsrGGkysyv8tos0k48j2w4L6'),
+('Anita', 'Jones', 'anita.jones@test.com', 'ajones', '$2a$12$sdtKiihA1lgHrCbIB3hxqul1i4HfBBUZedKjWDMJfD3anJrXPc8RK');
+
+INSERT INTO children (childname, parentid) 
+VALUES
+('Fred', 1),
+('Sally', 2),
+('Jane', 2);
+
+INSERT INTO flyers (title, flyerimage, eventlocation, eventstartdate, eventenddate, actiondate, flyeraction, flyercategory, parentuserid)
+VALUES
+('Corn Maze', 'data:image/jpeg;base64,' || pg_read_binary_file('/seeds/cornmazebase64.bin'), 'Best Corn Maze', '10/15/19 15:30','10/15/19 17:00','10/10/19','RSVP','school',1 ),
+('Field Trip', 'data:image/jpeg;base64,' || pg_read_binary_file('/seeds/afterschoolbase64.bin'), 'Washington D.C.', '11/13/19 13:00','11/13/19 15:00','9/5/19','Send Permission Slip','school',1 ),
+('Camping', 'data:image/jpeg;base64,' || pg_read_binary_file('/seeds/campingbase64.bin'), 'Camp Lost In the Woods', '9/3/19 9:30','9/4/19 10:30','10/20/19','Pay','school',2);
+
+INSERT INTO flyers_children (flyerid, childid)    
+VALUES 
+(1,1),
+(2,1),
+(3,2),
+(3,3);
+
--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://Flyer_Admin:sticky@localhost/flyer',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://Flyer_Admin:sticky@localhost/flyer-test',
-    CLIENT_ORIGIN: 'http://localhost:3000',
-    JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://dmac1108-a-flyer-on-the-wall.now.sh',
+    JWT_SECRET: process.env.JWT_SECRET || '$ervingItU9toyou'
 }
