60569457bb03677778b3469de1ddb532fbc87df8
Dawn McNeil <dawn.a.mcneil@gmail.com>
2019-12-01 19:40:09 -0500

    update client origin

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://Flyer_Admin:sticky@localhost/flyer',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://Flyer_Admin:sticky@localhost/flyer-test',
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://dmac1108-a-flyer-on-the-wall.now.sh',
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://a-flyer-on-the-wall.dawnamcneil.now.sh',
     JWT_SECRET: process.env.JWT_SECRET || '$ervingItU9toyou'
 }
